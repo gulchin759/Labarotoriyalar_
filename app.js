e58a44af8f3508edb9765c1cc1d86377749d507d
gulchin759 <mirzeyevagulcin81@gmail.com>
2026-03-11 23:57:13 +0400

    update

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ li_s.forEach((element) => {
         else if (element.textContent == '2') {
             window.location.href = 'lab2.html'
         }
+        else if (element.textContent == '3') {
+            window.location.href = 'lab3.html'
+        }
 
     })
 
@@ -18,34 +21,40 @@ li_s.forEach((element) => {
 
 
 
+let date = document.querySelector('#date-input');
+let out = document.querySelector('#out-input')
+
+date.addEventListener("input", () => {
+    let birthValue = date.value;
+    console.log(date.value);
 
 
 
+    let birthParts = birthValue.split("-");
+    let birthYear = parseInt(birthParts[0]);
+    let birthMonth = parseInt(birthParts[1]);
+    let birthDay = parseInt(birthParts[2]);
+
+    let today = new Date();
+    let todayYear = today.getFullYear();
+    let todayMonth = today.getMonth() + 1;
+    let todayDay = today.getDate();
+
+    let ageYears = todayYear - birthYear;
+    let ageMonths = todayMonth - birthMonth;
+    let ageDays = todayDay - birthDay;
 
 
+    if (ageDays < 0) {
+        ageMonths -= 1;
+        ageDays += new Date(todayYear, todayMonth - 1, 0).getDate();
+    }
 
-// salam
+    if (ageMonths < 0) {
+        ageYears -= 1;
+        ageMonths += 12;
+    }
+    out.value = `Sizin yaşayırsınız: ${ageYears} il, ${ageMonths} ay, ${ageDays} gün`;
+});
 
 
-
-
-// footer ul {
-//     display: flex;
-//     flex - direction: row;
-//     margin: 0 auto;
-//     margin - top: 2rem;
-//     width: 16rem;
-//     overflow - x: auto;
-//     margin - bottom: 1rem;
-// }
-
-// footer li {
-//     list - style: none;
-//     padding: 1rem;
-//     font - size: 1.5rem;
-//     font - weight: 900;
-//     border - radius: 0.5rem;
-//     background - color: rgb(77, 148, 15);
-//     margin - left: 0.3rem;
-//     color: white;
-// }
